e4416bd9b9eb7cde1e30d8178c11b9d2244ef165
Harper Foley <harper.e.foley@gmail.com>
2023-08-30 11:33:29 -0700

    Adds heroku to allowed image servers

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ module.exports = withStoreConfig({
       "medusa-public-images.s3.eu-west-1.amazonaws.com",
       "localhost",
       "medusa-server-testing.s3.amazonaws.com",
+      "https://little-goose-medusa-server-1656b4d85bc8.herokuapp.com",
     ],
   },
 })
